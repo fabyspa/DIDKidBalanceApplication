df9d89fa0aa8cddd6b6ffae982baa23715e8e4ee
Fabiana <fabyyy.spa@gmail.com>
2020-12-16 11:59:04 +0100

    Prova Comment

--- app/src/main/java/it/polito/ic2020/did_kidbalanceapplication/MainActivity.kt
+++ app/src/main/java/it/polito/ic2020/did_kidbalanceapplication/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //GIUSTO PER VEDERE SE FUNZIONA: VEDETE QUESTO?
+        //rispondete qui e poi commit e push
+        //Matte:
     }
 }

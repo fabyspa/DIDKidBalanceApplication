9c1ea8d4d03ff49fdfd721799cf126c2c399dce2
matteopellizzola <s238334@studenti.polito.it>
2020-12-16 12:03:54 +0100

    Aggiunto commento in main activity

--- app/src/main/java/it/polito/ic2020/did_kidbalanceapplication/MainActivity.kt
+++ app/src/main/java/it/polito/ic2020/did_kidbalanceapplication/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //GIUSTO PER VEDERE SE FUNZIONA: VEDETE QUESTO?
+        //mattepelliz vede
     }
 }

3a1766ae2aafd6e4937ea7439b1954da5f56eee0
matteopellizzola <s238334@studenti.polito.it>
2021-01-07 17:55:15 +0100

    Dati grafico Dinamici Manca gestione con File

--- app/src/main/java/it/polito/ic2020/did_kidbalanceapplication/GGraphFragment.kt
+++ app/src/main/java/it/polito/ic2020/did_kidbalanceapplication/GGraphFragment.kt
@@ -2,23 +2,12 @@ package it.polito.ic2020.did_kidbalanceapplication
 
 import android.graphics.Color
 import android.os.Bundle
-import android.os.SystemClock
-import android.provider.ContactsContract
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.core.graphics.red
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import com.jjoe64.graphview.GraphView
 import com.jjoe64.graphview.series.DataPoint
-import com.jjoe64.graphview.series.DataPointInterface
 import com.jjoe64.graphview.series.LineGraphSeries
-import it.polito.ic2020.did_kidbalanceapplication.databinding.FragmentGHomeBinding
-import it.polito.ic2020.did_kidbalanceapplication.databinding.FragmentGraphGBinding
 import kotlinx.android.synthetic.main.fragment_graph_g.*
-import kotlinx.android.synthetic.main.fragment_graph_g.view.*
-import kotlin.time.days
+import java.util.*
 
 /*class GGraphFragment : Fragment(){
     override fun onCreateView(
@@ -72,20 +61,80 @@ class GGraphFragment: Fragment(R.layout.fragment_graph_g){
             return DataPoint(a.toDouble(), b.toDouble())
         }
 
-        val points = arrayOf( DP (100,18),
-            DP (200,21),
-            DP(600,19),
-            DP(700,24)
+  /*      val points = arrayOf(DP((System.currentTimeMillis() / 1000) as Int, 18),
+                DP(200, 21),
+                DP(600, 19),
+                DP(700, 24)
         )
 
-        val series = LineGraphSeries<DataPoint>(points)
+   */
+        val l = mutableListOf<DataPoint>()
+        l.add(DP(Calendar.DAY_OF_MONTH-1, 18))
+        l.add(DP(Calendar.DAY_OF_MONTH-2, 20))
+        l.add(DP(Calendar.DAY_OF_MONTH-1, 22))
+        l.add(DP(Calendar.DAY_OF_MONTH as Int, 24))
+        l.add(DP(Calendar.DAY_OF_MONTH+1, 24))
+        l.add(DP(Calendar.DAY_OF_MONTH+2, 25))
+        l.add(DP(Calendar.DAY_OF_MONTH+3, 26))
+        l.add(DP(Calendar.DAY_OF_MONTH+4, 22))
+        l.add(DP(Calendar.DAY_OF_MONTH+5, 22))
+        l.add(DP(Calendar.DAY_OF_MONTH+6, 27))
+        l.add(DP(Calendar.DAY_OF_MONTH+7, 28))
+        l.add(DP(Calendar.DAY_OF_MONTH+8, 26))
+        l.add(DP(Calendar.DAY_OF_MONTH+9, 27))
+        l.add(DP(Calendar.DAY_OF_MONTH+10, 28))
+        l.add(DP(Calendar.DAY_OF_MONTH+11, 28))
+        l.add(DP(Calendar.DAY_OF_MONTH+12, 28))
+        l.add(DP(Calendar.DAY_OF_MONTH+13, 27))
+        l.add(DP(Calendar.DAY_OF_MONTH+14, 27))
+        l.add(DP(Calendar.DAY_OF_MONTH+15, 27))
+        l.add(DP(Calendar.DAY_OF_MONTH+16, 27))
+        l.add(DP(Calendar.DAY_OF_MONTH+17, 22))
+        l.add(DP(Calendar.DAY_OF_MONTH+18, 29))
+        l.add(DP(Calendar.DAY_OF_MONTH+19, 29))
+        l.add(DP(Calendar.DAY_OF_MONTH+20, 28))
+        l.add(DP(Calendar.DAY_OF_MONTH+21, 27))
+        l.add(DP(Calendar.DAY_OF_MONTH+22, 25))
+        l.add(DP(Calendar.DAY_OF_MONTH+23, 26))
+        l.add(DP(Calendar.DAY_OF_MONTH+24, 25))
+       /* l.removeFirst()
+        l.add(DP((System.currentTimeMillis()/1000) as Int,25))
+        val p = arrayOf(l.);
+        val s = LineGraphSeries<DataPoint>(p)
+
+        */
+        while(l.size>10) l.removeFirst() //needs 30gg
+
+        val dataPoints = arrayOfNulls<DataPoint>(l.size) // declare an array of DataPoint objects with the same size as your list
+
+        for (i in 0 until l.size) {
+            // add new DataPoint object to the array for each of your list entries
+            //dataPoints[i] = (i, l[i]) // not sure but I think the second argument should be of type double
+            dataPoints.set(i,l[i])
+        }
+
+        val s = LineGraphSeries(dataPoints) // This one should be obvious right? :)
+
+        println("l")
+        println(l)
+        println("dataP")
+        println(dataPoints)
+        println("s")
+        println(s)
+
+        /*val series = LineGraphSeries<DataPoint>(points)
         series.setColor(Color.CYAN)
         series.setDrawDataPoints(true);
         series.setDataPointsRadius(10F)
+         */
+        s.setColor(Color.CYAN)
+        s.setDrawDataPoints(true);
+        s.setDataPointsRadius(10F)
 
-        graph.addSeries(series)
-        graph.setBackgroundColor(Color.argb(100,255,91,0))
-        textView.text=("Ultima Pesata:  24Kg")
+        //graph.addSeries(series)
+        graph.addSeries(s)
+        graph.setBackgroundColor(Color.argb(100, 255, 91, 0))
+        textView.text=(l[l.lastIndex].toString())
 
         println("bella2")
 

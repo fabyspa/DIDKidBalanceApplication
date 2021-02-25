1e8dbd9aeb7a652c405a2a41519007aea6a5e146
matteopellizzola <s238334@studenti.polito.it>
2021-02-25 09:48:50 +0100

    Aggiunta callback quando premo indietro nella lista bambini genitore

--- app/src/main/java/it/polito/ic2020/did_kidbalanceapplication/AddChild/child_list_parentFragmenti.kt
+++ app/src/main/java/it/polito/ic2020/did_kidbalanceapplication/AddChild/child_list_parentFragmenti.kt
@@ -1,26 +1,21 @@
 package it.polito.ic2020.did_kidbalanceapplication.AddChild
 
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ListAdapter
-import android.widget.Toast
-import androidx.lifecycle.Observer
+import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import it.polito.ic2020.did_kidbalanceapplication.R
 import it.polito.ic2020.did_kidbalanceapplication.database.ChildWeightViewModel
-import it.polito.ic2020.did_kidbalanceapplication.database.HomeAdapter
 import it.polito.ic2020.did_kidbalanceapplication.database.childViewParentAdapter
 import kotlinx.android.synthetic.main.fragment_child_list_parent.view.*
 
-class child_list_parentFragment : Fragment() {
+
+class child_list_parentFragment : Fragment()   {
    lateinit var childWeightViewModel: ChildWeightViewModel
 
     override fun onCreateView(
@@ -38,10 +33,21 @@ class child_list_parentFragment : Fragment() {
 
         //UserViewModel
         childWeightViewModel = ViewModelProvider(this).get(ChildWeightViewModel::class.java)
-        childWeightViewModel.readAllData.observe(viewLifecycleOwner, {
-            user-> adapter.setData(user)
+        childWeightViewModel.readAllData.observe(viewLifecycleOwner, { user ->
+            adapter.setData(user)
         })
+        // This callback will only be called when MyFragment is at least Started.
+        // This callback will only be called when MyFragment is at least Started.
+        val callback: OnBackPressedCallback =
+            object : OnBackPressedCallback(true /* enabled by default */) {
+                override fun handleOnBackPressed() {
+                    // Handle the back button event
+                    findNavController().navigate(R.id.homeFragment)
+                }
+            }
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
 
+        // The callback can be enabled or disabled here or in handleOnBackPressed()
 
     return view
     }

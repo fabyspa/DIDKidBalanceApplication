6d4b333d897f71a2fe237dc3b45ac22415e30ca1
matteopellizzola <s238334@studenti.polito.it>
2021-02-25 15:29:35 +0100

    Aggiunto feedback mentre aspetto di entrare in game

--- app/src/main/java/it/polito/ic2020/did_kidbalanceapplication/MainActivity.kt
+++ app/src/main/java/it/polito/ic2020/did_kidbalanceapplication/MainActivity.kt
@@ -36,6 +36,8 @@ class MainActivity : AppCompatActivity() {
         val file2= File(this.filesDir?.absolutePath,"answer.txt")
         val nameFileExists = file1.exists()
         val ansFileExists=file2.length().toInt()
+        println("file answer exists: "+ file2.exists())
+        println("file size: "+file2.length().toInt())
         if(nameFileExists&&ansFileExists!=0){
             graph.startDestination = R.id.GHomeFragment3
         } else {
--- app/src/main/java/it/polito/ic2020/did_kidbalanceapplication/database/HomeAdapter.kt
+++ app/src/main/java/it/polito/ic2020/did_kidbalanceapplication/database/HomeAdapter.kt
@@ -1,13 +1,19 @@
 package it.polito.ic2020.did_kidbalanceapplication.database
 
 import android.content.Intent
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import it.polito.ic2020.did_kidbalanceapplication.ChildActivity
 import it.polito.ic2020.did_kidbalanceapplication.R
 import kotlinx.android.synthetic.main.card_view.view.*
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.coroutines.delay
+import java.util.*
+import kotlin.concurrent.schedule
 
 class HomeAdapter: RecyclerView.Adapter<HomeAdapter.MyViewHolder>(){
     private var userList= emptyList<ChildWeight>()
@@ -26,15 +32,19 @@ class HomeAdapter: RecyclerView.Adapter<HomeAdapter.MyViewHolder>(){
         holder.itemView.picture.setImageResource(currentItem.picture)
         holder.itemView.setOnClickListener( View.OnClickListener() {
             //findNavController(holder.itemView).navigate(R.id.action_homeFragment_to_navigation_child)
+            holder.itemView.loading.isVisible = true
             val `in` = Intent(holder.itemView.context, ChildActivity::class.java)
             `in`.putExtra("name", currentItem.nome)
             `in`.putExtra("id", currentItem.id)
             println(`in`.extras.toString())
             println("ciao")
             holder.itemView.context.startActivity(`in`)
-            }
-
-        )
+            Handler().postDelayed({
+                //doSomethingHere()
+                holder.itemView.loading.isVisible = false
+                println("porta puttana")
+            }, 1000)
+        })
 
     }
 

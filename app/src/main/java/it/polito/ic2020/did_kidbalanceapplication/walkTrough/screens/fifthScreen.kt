56ce39da7f29da213fab8629d785467425151d12
Fabiana <fabyyy.spa@gmail.com>
2021-03-31 16:35:19 +0200

    Cambio Profilo ( per adesso una sola immagine) 2

--- app/src/main/java/it/polito/ic2020/did_kidbalanceapplication/walkTrough/screens/fifthScreen.kt
+++ app/src/main/java/it/polito/ic2020/did_kidbalanceapplication/walkTrough/screens/fifthScreen.kt
@@ -1,16 +1,18 @@
 package it.polito.ic2020.did_kidbalanceapplication.walkTrough.screens
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import it.polito.ic2020.did_kidbalanceapplication.R
+import kotlinx.android.synthetic.main.fragment_fifth_screen.*
 import kotlinx.android.synthetic.main.fragment_fifth_screen.view.*
 
 class fifthScreen : Fragment() {
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,14 +22,22 @@ class fifthScreen : Fragment() {
         val view =  inflater.inflate(R.layout.fragment_fifth_screen, container, false)
 
         view.Gioca.setOnClickListener {
-           // findNavController().navigate(R.id)
+            //Toast.makeText(this.context,"Ok bene",Toast.LENGTH_LONG).show()
+            onBoardingFinished()
+            waitGame.visibility=View.VISIBLE
+            findNavController().navigate(R.id.action_viewPagerFragment2_to_gameCircle2)
         }
 
         return view
     }
 
     private fun onBoardingFinished(){
-        //setsharedPref =
+        val id = requireActivity().intent!!.extras?.get("id").toString().toInt()
+        println("from fifth: "+id)
+        val sharedPref = requireActivity().getSharedPreferences("onBoarding"+id,Context.MODE_PRIVATE)
+        val editor = sharedPref.edit()
+        editor.putBoolean("Finished",true)
+        editor.apply()
     }
 
 }

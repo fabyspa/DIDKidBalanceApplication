e8c2ec5e1b7fe346ce15c7285c8f820924d3ef30
Fabiana <fabyyy.spa@gmail.com>
2021-04-12 19:09:04 +0200

    Cambio profilo funziona

--- app/src/main/java/it/polito/ic2020/did_kidbalanceapplication/ChangePictureFragment.kt
+++ app/src/main/java/it/polito/ic2020/did_kidbalanceapplication/ChangePictureFragment.kt
@@ -61,6 +61,69 @@ class ChangePictureFragment : Fragment() {
     ): View? {
         // val myValue = arguments?.get("id")
         binding = DataBindingUtil.inflate<FragmentChangePictureBinding>(inflater, R.layout.fragment_change_picture, container, false)
+
+        val grid: androidx.gridlayout.widget.GridLayout = binding.gridLayout
+        val childCount: Int = grid.childCount
+
+        for (i in 0 until childCount) {
+            grid.getChildAt(i).setOnClickListener {
+                lifecycleScope.launch(Dispatchers.IO) {
+                    val db: ChildWeightDatabase = ChildWeightDatabase.getInstance(requireContext())
+                    //if (param1 != -1) {
+                    if (arguments?.getInt("id") != null) {
+                        val id = arguments?.getInt("id")
+                        println("argument: " + id)
+                        //println("ChangePictureFragment$param1")
+                        val child = db.childDataBaseDao().getAllChildData(requireArguments().getInt("id").toInt())
+                        when (i) {
+                            0 -> child.picture = R.drawable.ic_b_half_moon
+                            1 -> child.picture = R.drawable.ic_b_planet_earth
+                            2 -> child.picture = R.drawable.ic_b_ufo
+                            3 -> child.picture = R.drawable.ic_f
+                            4 -> child.picture = R.drawable.ic_b_monkey
+                            5 -> child.picture = R.drawable.ic_m
+                            6 -> child.picture = R.drawable.ic_b_satellite
+                            7 -> child.picture = R.drawable.ic_b_telescope
+                            8 -> child.picture = R.drawable.ic_b_rocket
+                            else -> R.drawable.kid
+
+                        }
+                        db.childDataBaseDao().update(child)
+                        println(binding.changeProf.id)
+                        withContext(Dispatchers.Main){
+                            activity?.userPicture?.setImageResource(
+                                    when (i) {
+                                        0 ->  R.drawable.ic_b_half_moon
+                                        1 ->  R.drawable.ic_b_planet_earth
+                                        2 ->  R.drawable.ic_b_ufo
+                                        3 ->  R.drawable.ic_f
+                                        4 ->  R.drawable.ic_b_monkey
+                                        5 ->  R.drawable.ic_m
+                                        6 ->  R.drawable.ic_b_satellite
+                                        7 ->  R.drawable.ic_b_telescope
+                                        8 ->  R.drawable.ic_b_rocket
+                                        else -> R.drawable.kid
+                                    })
+                            findNavController().navigateUp()
+                        }
+
+                        Log.i("CPF","IMAGE"+ binding.changeProf.drawable)
+
+
+
+                    }
+                    else {
+                        withContext(Dispatchers.Main) {
+                            Toast.makeText(activity, "Error!", Toast.LENGTH_SHORT).show()
+                        }
+
+                    }
+
+                }
+
+            }
+        }
+
         binding.changeProf.setOnClickListener {
             println("Click")
             Toast.makeText(this.activity, "Image updated!", Toast.LENGTH_SHORT).show()
